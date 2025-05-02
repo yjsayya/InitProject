1ee0ef0a69a9585ab2a6f6a350d88691ce44567f
yjsayya <djdj9876@naver.com>
2025-05-02 23:37:39 +0900

    feat: JsonUtil 추가

---
+++ src/main/java/init/project/global/utils/JsonUtil.java
@@ -0,0 +1,34 @@
+package init.project.global.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+
+public class JsonUtil {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private JsonUtil() { }
+
+    public static ObjectMapper getInstance() {
+        return objectMapper;
+    }
+
+    public static Map<String, String> toMap(String json) {
+        try {
+            return objectMapper.readValue(json, Map.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to parse JSON to Map", e);
+        }
+    }
+
+    public static String toJson(Object obj) {
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to convert Object to JSON", e);
+        }
+    }
+
+}
