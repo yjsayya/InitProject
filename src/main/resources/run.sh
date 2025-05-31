d06a7be7be6af801ebb182b0beb3227ef967b37b
sayya <djdj9876@naver.com>
2025-05-31 21:45:13 +0900

    feat: run.sh에 run() 메서드와 print() 메서드 추가

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 # 1. Java Home
-JAVA_HOME=/app/java/jdk1.8.0 # java 경로 넣기
+JAVA_HOME=/app/java/jdk1.8.0  # JAVA 설치 경로 수정 필요
 
-# 2. Application
-APP_HOME=`pwd` # 현재 dir
-APP_NAME="jar파일이름" # BootJar 후에 생긴 .jar 파일
-APPLICATION=$APP_HOME/$APP_NAME
-SERVICE_NAME=서비스이름
-
-#CONF=$APP_HOME/application.yml
-
-# 4.PID
-PID_FILE=$SERVICE_NAME.pid
+# 2. Application 설정
+APP_HOME=$(pwd)
+APP_NAME="jar파일이름"  # 실제 JAR 파일 이름으로 수정 필요
+APPLICATION="$APP_HOME/$APP_NAME"
+SERVICE_NAME=서비스이름  # 서비스 이름 지정
 
 # 3. Java Options
 MIN_HEAP_MEMORY=1024m
@@ -22,13 +17,19 @@ JAVA_OPTS="-server"
 JAVA_OPTS="$JAVA_OPTS -Dservice.name=$SERVICE_NAME"
 JAVA_OPTS="$JAVA_OPTS -Xms$MIN_HEAP_MEMORY -Xmx$MAX_HEAP_MEMORY"
 JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=dev"
-#JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=prod"
+# JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=prod"
 
+# 4. PID 파일
+PID_FILE="$SERVICE_NAME.pid"
+
+# 5. 실행 명령어
 JAVA_CMD="$JAVA_HOME/bin/java $JAVA_OPTS -jar $APPLICATION"
+
+
 
 start() {
   if [ -f "$PID_FILE" ] && kill -0 $(cat "$PID_FILE") 2>/dev/null; then
-    echo "$SERVICE_NAME Is Already Running with PID $(cat $PID_FILE)."
+    echo "$SERVICE_NAME is already running with PID $(cat $PID_FILE)."
     exit 1
   fi
   echo "Starting $SERVICE_NAME..."
@@ -40,9 +41,9 @@ start() {
 stop() {
   if [ -f "$PID_FILE" ]; then
     PID=$(cat "$PID_FILE")
-    if kill -0 $PID 2>/dev/null; then
+    if kill -0 "$PID" 2>/dev/null; then
       echo "Stopping $SERVICE_NAME with PID $PID..."
-      kill $PID
+      kill "$PID"
       rm -f "$PID_FILE"
       echo "$SERVICE_NAME stopped."
     else
@@ -62,6 +63,20 @@ status() {
   fi
 }
 
+run() {
+  if [ -f "$PID_FILE" ] && kill -0 $(cat "$PID_FILE") 2>/dev/null; then
+    echo "$SERVICE_NAME is already running with PID $(cat $PID_FILE)."
+    exit 1
+  fi
+  echo "Running $SERVICE_NAME in foreground..."
+  exec $JAVA_CMD
+}
+
+print() {
+  echo "Java CMD:"
+  echo "$JAVA_CMD"
+}
+
 case "$1" in
   start)
     start
@@ -72,8 +87,14 @@ case "$1" in
   status)
     status
     ;;
+  run)
+    run
+    ;;
+  print)
+    print
+    ;;
   *)
-    echo "Usage: $0 {start|stop|status}"
+    echo "Usage: $0 {start|stop|status|run|print}"
     exit 1
     ;;
 esac
